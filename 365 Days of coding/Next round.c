878345198e3ec850ffaad65f69cbe5d29feb5ba7
Zain Abbas <zainuchiha16@gmail.com>
2023-01-05 18:22:31 +0500

    streak

---
+++ 365 Days of coding/Next round.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100];
+    int n,k,i;
+    scanf("%d %d", &n, &k);
+    fflush(stdin);
+for (i = 0; i < n; i++)
+{
+    scanf("%d", &a[i]);
+}
+
+
+
+
+}
